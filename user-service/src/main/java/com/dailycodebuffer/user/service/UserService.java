7766563f362da6c251832f04449e1028fa31a004
Maksudur Rahman Maruf <43263023+maksudur-rahman-maruf@users.noreply.github.com>
2021-09-24 22:37:58 +0600

    Add files via upload

---
+++ user-service/src/main/java/com/dailycodebuffer/user/service/UserService.java
@@ -0,0 +1,66 @@
+package com.dailycodebuffer.user.service;
+
+import com.dailycodebuffer.user.VO.Department;
+import com.dailycodebuffer.user.VO.ResponseTemplateVO;
+import com.dailycodebuffer.user.entity.User;
+import com.dailycodebuffer.user.repository.UserRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@Slf4j
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public User saveUser(User user) {
+        log.info("Inside saveUser method of UserService");
+        return userRepository.save(user);
+    }
+
+// Before Adding Circuit Breaker
+//    public ResponseTemplateVO getUserWithDepartment(Long userId) {
+//        log.info("Inside getUserWithDepartment method of UserService");
+//        ResponseTemplateVO vo = new ResponseTemplateVO();
+//        User user = userRepository.findByUserId(userId);
+//
+//        Department department =
+//                restTemplate.getForObject("http://DEPARTMENT-SERVICE/departments/" + user.getDepartmentId(),
+//                        Department.class);
+//
+//        vo.setUser(user);
+//        vo.setDepartment(department);
+//
+//        return vo;
+//    }
+
+    @CircuitBreaker(name="userService", fallbackMethod = "userFallBack")
+    public ResponseEntity getUserWithDepartment(Long userId) {
+        log.info("Inside getUserWithDepartment method of UserService");
+        ResponseTemplateVO vo = new ResponseTemplateVO();
+        User user = userRepository.findByUserId(userId);
+        if(user == null) return new ResponseEntity("Can't find the user", HttpStatus.OK);
+
+        Department department =
+                restTemplate.getForObject("http://DEPARTMENT-SERVICE/departments/" + user.getDepartmentId(),
+                        Department.class);
+
+        vo.setUser(user);
+        vo.setDepartment(department);
+
+        return new ResponseEntity(vo, HttpStatus.OK);
+    }
+
+   public ResponseEntity userFallBack(Exception e) {
+        return new ResponseEntity("Department Service is Down!!!", HttpStatus.NOT_FOUND);
+   }
+}
