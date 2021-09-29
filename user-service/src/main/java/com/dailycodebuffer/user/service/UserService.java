c2e3ea96abe1ffe5f330b69c26f9455aa8267b78
Maksudur Rahman Maruf <43263023+maksudur-rahman-maruf@users.noreply.github.com>
2021-09-29 14:55:01 +0600

    Adding Bulkhead Annotation and Default aspect Ordering

--- user-service/src/main/java/com/dailycodebuffer/user/service/UserService.java
+++ user-service/src/main/java/com/dailycodebuffer/user/service/UserService.java
@@ -45,7 +45,8 @@ public class UserService {
 
     @CircuitBreaker(name="userService", fallbackMethod = "userFallBack")
     @RateLimiter(name="userService")
-//    @Retry(name="userService", fallbackMethod = "userFallBack") 
+//    @Retry(name="userService", fallbackMethod = "userFallBack")   // Default Aspect Order: BulkHead, RateLimiter, Circuit Breaker, Retry
+    @BulkHead(name="userService", fallbackMethod = "userFallBack")
     public ResponseEntity getUserWithDepartment(Long userId) {
         log.info("Inside getUserWithDepartment method of UserService");
         ResponseTemplateVO vo = new ResponseTemplateVO();
